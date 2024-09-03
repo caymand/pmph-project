8e1459752c185624e818c316a3976aad614397b0
TheRune22 <rune.e.b.lejboelle@gmail.com>
2024-09-03 14:55:20 +0200

    Fixed swizzle pattern

--- access_patterns/main.py
+++ access_patterns/main.py
@@ -3,9 +3,9 @@ import numpy as np
 from colorama import Back
 
 X = 8
-Y = 4
+Y = 8
 
-colors = {0: Back.BLUE, 1: Back.GREEN, 2: Back.YELLOW, 3: Back.RED}
+colors = {0: Back.BLUE, 1: Back.GREEN, 2: Back.YELLOW, 3: Back.RED, 4: Back.CYAN, 5: Back.MAGENTA, 6: Back.WHITE, 7: Back.BLACK}
 
 def print_cell(x, y):
     print(colors[y] + f"({x*X:02}...{(x+1) * X - 1:02}, {y})", end=Back.RESET + "\t")
@@ -22,33 +22,11 @@ for y in range(Y):
 
 print("\n\n\n")
 
-# Option 1
-# def global_shared_map(x, y):
-#     new_y = (x & 1) | ((x >> 1) & 2)
-#     new_x = ((x << 1) & 4) | y ^ new_y
-#     return new_x, new_y
-
 # Option 2
 def global_shared_map(x, y):
-    new_y = x // 2
-    row_index = (x * 4 + y) % 8
-    new_x = row_index
+    new_y = x
+    new_x = y ^ new_y
 
-    if new_y % 2 == 1:
-        if row_index % 2 == 0:
-            new_x += 1
-        else:
-            new_x -= 1
-
-    # new_x = new_x ^ (y & 2)
-    # TODO: use XOR instead
-    if new_y // 2 != 0:
-        if y // 2 != 0:
-            new_x -= 2
-        else:
-            new_x += 2
-
-    # new_x = row_index + (new_y % 2) * (- (row_index % 2) + (1 - (row_index % 2))) + (new_y // 2) * 2 * (-(row_index // 2) + (1 - (row_index // 2)))
     return new_x, new_y
 
 shared_global_map = {}
@@ -69,40 +47,10 @@ for y in range(Y):
 
 print("\n\n\n")
 
-# MMA matrices
-# for matrix in range(4):
-#     for row in range(8):
-#         row_x, row_y = shared_global_map[(row, matrix)]
-#         print_cell(row_x, row_y)
-#         print_newline()
-#
-#     print("\n\n\n")
 
-# for row in range(4):
-#     for cell in range(2):
-#         row_x, row_y = (cell, row)
-#         print_cell(row_x, row_y)
-#     print_newline()
-
-
-# Load each row as separate matrix
-for matrix in range(4):
-    for row in range(8):
-        new_x, new_y = shared_global_map[row, matrix]
-        print_cell(new_x, new_y)
-        print_newline()
-
-    print("\n\n\n")
-
-
-# test = [[f"({row // 4 * 8 + col}, {row % 4})" for col in range(8)] for row in range(8)]
-# print(np.array(test).T)
-
-
-# for matrix in range(4):
-#     for row in range(8):
-#         print_cell(row, matrix)
-#         print_newline()
-#
-#     print("\n\n\n")
+for y in range(Y):
+    for x in range(X):
+        src_x, src_y = shared_global_map[(x, y)]
+        print_cell(src_x, src_y ^ src_x)
+    print_newline()
 
